33519ea26e3f54d8cd7ea6895a773f36012419f7
GODSeye <137390883+GODseye17@users.noreply.github.com>
2025-04-28 00:14:17 +0530

    port fixed

--- main.py
+++ main.py
@@ -36,3 +36,6 @@ def get_query_response(request: QueryRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
